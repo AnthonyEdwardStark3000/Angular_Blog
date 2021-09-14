2d5461cb5478e032bf28a17e3e84b978ed0aa79b
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2021-09-14 20:57:58 +0530

    Adding works about Angular blogs but an minor error is there at add-blog component.ts file

---
+++ src/app/add-blog/add-blog.component.ts
@@ -0,0 +1,28 @@
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-blog',
+  templateUrl: './add-blog.component.html',
+  styleUrls: ['./add-blog.component.css']
+})
+export class AddBlogComponent implements OnInit {
+
+  constructor() { }
+  blogs:any[]=[]
+  ngOnInit(): void {
+  }
+
+  clicked(title:any,content:any)
+  {
+    let blog={"title":title.value,"content":content.value};
+    if(localStorage.getItem("blogs"))
+    {
+      this.blogs=JSON.parse(localStorage.getItem("blogs"))
+    }
+    this.blogs.push(blog)
+    localStorage.setItem("blogs",JSON.stringify(this.blogs))
+  }
+
+
+}
