523ce28c67e787524acab3a9ca213d01bf8bb400
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2021-09-14 23:01:55 +0530

    finally done with the basic app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  blogs:any[]=[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.blogs=JSON.parse(localStorage.getItem("blogs")||'{}');
+    console.log(this.blogs);
+  }
+
+  remove(blog: any)
+  {
+    let index= this.blogs.indexOf(blog);
+    this.blogs.splice(index,1);
+    localStorage.setItem("blogs",JSON.stringify(this.blogs));
+    console.log("Item has been deleted");
   }
 
 }
